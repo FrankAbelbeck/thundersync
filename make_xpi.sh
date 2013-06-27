32b6b846096c51de44476033873d4e9337e02751
Frank Abelbeck <frank.abelbeck@googlemail.com>
2013-06-27 19:20:35 +0000

    Fixes regarding version (b-eta instead of a-lpha); fixed make_xpi.sh (file list)

--- make_xpi.sh
+++ make_xpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-zip -r thundersync-2.2a.xpi \
+zip -r thundersync-2.2b.xpi \
 	chrome.manifest \
 	README \
 	COPYING \
@@ -9,19 +9,25 @@ zip -r thundersync-2.2a.xpi \
 	content/ThunderSyncDialog.css \
 	content/ThunderSyncDialog.js \
 	content/ThunderSyncDialog.xul \
+	content/ThunderSyncMenuItem.js \
 	content/ThunderSyncMenuItem.xul \
+	content/ThunderSyncPreferences.css \
 	content/ThunderSyncPreferences.js \
 	content/ThunderSyncPreferences.xul \
+	content/ThunderSyncPreferencesIMAP.js \
+	content/ThunderSyncPreferencesIMAP.xul \
 	content/ThunderSyncToolbar.xul \
 	content/ThunderSyncVCardLib.js \
 	locale/de/ThunderSyncDialog.dtd \
 	locale/de/ThunderSyncDialog.properties \
 	locale/de/ThunderSyncMenuItem.dtd \
+	locale/de/ThunderSyncMenuItem.properties \
 	locale/de/ThunderSyncPreferences.dtd \
 	locale/de/ThunderSyncPreferences.properties \
 	locale/en/ThunderSyncDialog.dtd \
 	locale/en/ThunderSyncDialog.properties \
 	locale/en/ThunderSyncMenuItem.dtd \
+	locale/en/ThunderSyncMenuItem.properties \
 	locale/en/ThunderSyncPreferences.dtd \
 	locale/en/ThunderSyncPreferences.properties \
 	skin/icon.png \
